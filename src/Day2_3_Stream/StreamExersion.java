ab16d3d592abdc79fcbf4ccb6340ba30df25fc79
fa1seW <wanganji620@gmail.com>
2022-11-12 21:00:51 +0800

    StreamTerminate

--- src/Day2_3_Stream/StreamExersion.java
+++ src/Day2_3_Stream/StreamExersion.java
@@ -2,12 +2,14 @@ package Day2_3_Stream;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class StreamExersion extends StreamCreation{
@@ -40,5 +42,39 @@ public class StreamExersion extends StreamCreation{
         Stream<Integer> peekInts = Stream.of(ints)
                 .peek(e-> System.out.println("Fetching: "+e));
         show("peekInts",peekInts);
+    }
+
+    @Test
+    public void terminateStream() throws IOException {
+
+        // count, max, min 都是约简方式
+        var contents = Files.readString(Paths.get("./src/Day2_3_Stream/A.txt"));
+        List<String> words = List.of(contents.split("\\PL+"));
+        long count = words.stream().filter(w->w.length()>7).count();
+        System.out.println(count);
+
+        // 使用 max + Optional<String> 约简
+        Stream<String> wordsStream = words.stream();
+        Optional<String> max = wordsStream.max(String::compareToIgnoreCase);
+        System.out.println(max);
+
+        // 使用 min + Optional<Integer> 约简
+        ArrayList<Integer> ints = new ArrayList<>();
+        ints.add(5);
+        ints.add(6);
+        ints.add(3);
+        Optional<Integer> maxInteger = ints.stream().min(Integer::compareTo);
+        System.out.println(maxInteger);
+
+        // 使用findFirst找到第一个满足条件的值
+        Stream<String> wordsStream2 = words.stream(); // 注意：需要新建一个流，不能使用已经约简的流！
+        Optional<String> startWith = wordsStream2.filter(e -> e.startsWith("e")).findFirst();
+        // startsWith区分大小写，找不到会return Optional.empty
+        System.out.println(startWith);
+
+        // findAny找到任意一个匹配的值，在并行处理时有效
+        Stream<String> wordsStream3 = words.stream();
+        Optional<String> startAny = wordsStream3.filter(e -> e.startsWith("e")).findAny();
+        System.out.println(startAny);
     }
 }
